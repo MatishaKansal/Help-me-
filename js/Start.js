6108ca897498c28c78d28a34850c69262f97b9e9
MatishaKansal <64193940+MatishaKansal@users.noreply.github.com>
2020-07-26 18:13:06 +0530

    Add files via upload

--- js/Start.js
+++ js/Start.js
@@ -4,19 +4,28 @@ class Start{
         this.food.style('height', '150px');
         this.food.style('width', '150px');
         this.food.style('color', 'black');
+        this.food.style('fontSize', 'x-large');
+        this.food.style('fontWeight', '900');
         this.food.style('background', 'orange');
         this.ngo = createButton("NGO");
         this.ngo.style('height', '150px');
         this.ngo.style('width', '150px');
         this.ngo.style('color', 'black');
+        this.ngo.style('fontSize', 'x-large');
+        this.ngo.style('fontWeight', '900');
         this.ngo.style('background', 'rgb(22, 150, 248)');
         this.delivery = createButton("Volunteer For Delivery");
         this.delivery.style('height', '150px');
         this.delivery.style('width', '150px');
         this.delivery.style('color', 'black');
-        this.delivery.style('background', 'green');
+        this.delivery.style('fontSize', 'x-large');
+        this.delivery.style('fontWeight', '900');
+        this.delivery.style('background', 'rgb(108, 247, 0)');
         this.greeting = createElement("h1");
         this.greeting.style('color', 'darkBlue');
+        this.greeting.style('fontSize', 'xxx-large');
+        this.greeting.style('fontWeight', '900');
+        this.greeting.style('textDecoration', 'underline overline');
 
     }
 
@@ -30,7 +39,7 @@ class Start{
     display(){
         this.greeting.style.color = "purple";
         this.greeting.html("HELP ME!");
-        this.greeting.position(380, 50);
+        this.greeting.position(380, 30);
         this.food.position(200, 200);
         this.ngo.position(400, 400);
         this.delivery.position(600, 200);
--- js/Time.js
+++ js/Time.js
@@ -1,6 +1,11 @@
 class Time{
     constructor(){
-        this.button = createButton(game5.getItemInfo());
+        this.button = createButton('game5.getItemInfo()         game5.getItemInfo');
+        this.button.style('wordBreak', 'break-all');
+        this.button.style('color', 'green');
+        this.button.style('width', '150px');
+        this.button.style('height', '100px');
+
     }
 
     hide(){
